0782f66d8a55e514ce87f78fe1420579240cacd3
Roy Merkel <roy_d_merkel@hotmail.com>
2022-03-20 15:54:04 -0700

    Update err.h
    Added detection for c++ and changes to imports accordingly.

--- err.h
+++ err.h
@@ -2,6 +2,9 @@
 #define ERR_H
 #include <stdarg.h>
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
 void err(int eval, const char *fmt, ...);
 void errx(int eval, const char *fmt, ...);
 void warn(const char *fmt, ...);
@@ -10,5 +13,9 @@ void verr(int eval, const char *fmt, va_list args);
 void verrx(int eval, const char *fmt, va_list args);
 void vwarn(const char *fmt, va_list args);
 void vwarnx(const char *fmt, va_list args);
+#if defined(__cplusplus)
+}
+#endif
 
+  
 #endif // #ifndef ERR_H
